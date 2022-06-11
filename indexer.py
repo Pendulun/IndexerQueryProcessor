66c52b09b46fe9e072478375a36b0e2efc2ae8c7
Daniel Campos <danielcampos@dcc.ufmg.br>
2022-06-11 19:31:55 -0300

    Limitando num warc e docs

--- indexer.py
+++ indexer.py
@@ -2,6 +2,7 @@ import sys
 import resource
 import argparse
 import pathlib
+import logging
 from warcio import ArchiveIterator
 from indexClasses.index import *
 
@@ -17,22 +18,37 @@ def main(my_args):
     corpus_dir = pathlib.Path(my_args.corpus_path)
 
     assert corpus_dir.exists()
-    
-    #Ler um arquivo warc
+
+    index_dir_path = pathlib.Path(my_args.index_path)
+    index_dir_path.mkdir(parents=True, exist_ok=True)
+
+    assert index_dir_path.exists()
 
     index = Index()
 
+    WARC_FILE_LIMIT = 2
+    DOC_PER_FILE_LIMIT = 2
+    
+    warc_count = 0
     for file in corpus_dir.glob('*.warc.gz.kaggle'):
+        doc_count = 0
+        warc_count+=1
 
         with open(file, 'rb') as stream:
             for record in ArchiveIterator(stream):
                 if record.rec_type == 'response':
                     texto = record.raw_stream.read().decode()
                     if texto.strip() != "" and len(texto.strip()) > 10:
-                        print(texto)
-                        break
+                        doc_count +=1
+                        logging.info(f"WARC FILE: {warc_count}, DOC: {doc_count}")
+                        logging.info(f"\n{texto}")
+                        
+
+                if doc_count == DOC_PER_FILE_LIMIT:
+                    break
         
-        break
+        if warc_count == WARC_FILE_LIMIT:
+            break   
 
     #Para cada documento, calcular a frequência de palavras e adicionar no index
     #Tudo isso levando em consideração a memória utilizada
@@ -67,6 +83,8 @@ def configArgs(parser):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser = configArgs(parser)
+    logging.basicConfig(level=logging.INFO, format='%(thread)d-%(threadName)s-%(levelname)s-%(message)s',
+    filename="log.log", filemode="w")
 
     my_args = parser.parse_args()
 
